d3ee066494b6d8d4ecaea9e34b073f71e10cb1f3
Islam_Saber <eng.islam_saber@yahoo.com>
2024-01-26 08:02:24 +0200

    Adding TestBase class

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,5 +1,24 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestBase {
 
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void startDriver()
+	{
+		driver = WebDriverManager.chromedriver().create();
+	}
+	
+	@AfterSuite
+	public void stopDriver()
+	{
+		driver.quit();
+	}
 }
